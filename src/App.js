f91b562a43fbca88a97deabbe1c3ba07f477cabc
IvanVlad1997 <ivanvlad97@yahoo.ro>
2020-09-13 11:33:24 +0300

    Added folders for every component/page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
-import Login from './pages/Login';
-import Page404 from './pages/Page404';
-import Category from './pages/Category';
-import Cart from './pages/Cart';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Page404 from './pages/Page 404/Page404';
+import Category from './pages/Category/Category';
+import Cart from './pages/Cart/Cart';
 import './utils/utility-classes.css';
-import Product from './pages/Product';
+import Product from './pages/Product/Product';
 
 function App() {
   return(
--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCcbZMkwd4ZV_1DiH0R3TZFCg9GKaIMeoM",
-    authDomain: "react-sirluggia-shop.firebaseapp.com",
-    databaseURL: "https://react-sirluggia-shop.firebaseio.com",
-    projectId: "react-sirluggia-shop",
-    storageBucket: "react-sirluggia-shop.appspot.com",
-    messagingSenderId: "184253035461",
-    appId: "1:184253035461:web:20fe5c173c7675c74c51ac"
-};
-
+    apiKey: "AIzaSyCxmVuDNR5WVSwvH9yU1j6glNK0F0u_eNk",
+    authDomain: "firedux-todo-aec0f.firebaseapp.com",
+    databaseURL: "https://firedux-todo-aec0f.firebaseio.com",
+    projectId: "firedux-todo-aec0f",
+    storageBucket: "firedux-todo-aec0f.appspot.com",
+    messagingSenderId: "925731692165",
+    appId: "1:925731692165:web:a4987a5e76736a1fe0d3ec",
+    measurementId: "G-5RZFGHYD41"
+  };
+  
 export default firebaseConfig;
