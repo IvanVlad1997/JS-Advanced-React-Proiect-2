f72ba0a9e24cb92b6498f0377c5d9952db2cbab5
IvanVlad1997 <ivanvlad97@yahoo.ro>
2020-09-14 11:40:32 +0300

    Without firebase config

---
+++ src/configs/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCxmVuDNR5WVSwvH9yU1j6glNK0F0u_eNk",
+  authDomain: "firedux-todo-aec0f.firebaseapp.com",
+  databaseURL: "https://firedux-todo-aec0f.firebaseio.com",
+  projectId: "firedux-todo-aec0f",
+  storageBucket: "firedux-todo-aec0f.appspot.com",
+  messagingSenderId: "925731692165",
+  appId: "1:925731692165:web:a4987a5e76736a1fe0d3ec",
+  measurementId: "G-5RZFGHYD41"
+};
+  
+export default firebaseConfig;
