3d88c8753fe6cf4f2f10ba4ef67cdc0ca3b211cf
IvanVlad1997 <ivanvlad97@yahoo.ro>
2020-09-14 13:02:34 +0300

    ProductItemRemoveFromFavorite

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './ProductItem.css';
 import { connect } from 'react-redux';
 import { addToCart } from '../../redux/cart/cartActions';
-import {addToFavorite} from '../../redux/favorite/favoriteActions'
+import {addToFavorite, removeFromFavorite} from '../../redux/favorite/favoriteActions'
 import { Link } from 'react-router-dom';
 
 function ProductItem(props) {
     const {name, price, currency, image, id} = props;
-    const favorit= false
+    
+    const thereIs = props.favorites.favorites.some((el) => el.id === id)
+      let favorit
+    if  (thereIs) favorit= true
+      else favorit= false
+  
     return(
         <div className="product-item col-12 col-md-4 mb-3 d-flex flex-column align-items-center">
             <Link to={`/product/${id}`} className="d-flex flex-column align-items-center">
@@ -33,16 +38,11 @@ function ProductItem(props) {
             { favorit 
                 ?    (<button
                         className="btn btn-outline-dark"
-                        onClick={() => props.addToCart({
-                            product: {
-                                id,
-                                name,
-                                price,
-                                currency,
-                                image
-                            }
-                        })}
+                        onClick={() => props.removeFromFavorite({
+                            id
+                       })}
                     >
+
                         Scoate de la favorite
                     </button>)
                 :  <button
@@ -69,8 +69,15 @@ function ProductItem(props) {
 function mapDispatchToProps(dispatch) {
     return {
         addToCart: (product) => dispatch(addToCart(product)),
-        addToFavorite: (favorite) => dispatch(addToFavorite(favorite))
+        addToFavorite: (favorite) => dispatch(addToFavorite(favorite)),
+        removeFromFavorite: (favorite) => dispatch(removeFromFavorite(favorite))
     };
 }
 
-export default connect(null, mapDispatchToProps)(ProductItem);
+const mapStateToProps = (state) => {
+    return {
+        favorites: state.favorites
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductItem);
